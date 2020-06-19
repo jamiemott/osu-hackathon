f3c2323a5a329115ac76b8be3d3f342b58ecd7b2
Jamie Mott <jamie.c.mott@gmail.com>
2020-06-19 16:41:27 -0600

    removed POST method

--- app/main.py
+++ app/main.py
@@ -26,12 +26,12 @@ def books():
 def links():
     return render_template('links.html')
 
-@app.route('/insert', methods=['POST'])
+@app.route('/insert')
 def insertDB():
     # prepare a cursor object using cursor() method
     db = conn.cursor()
 
-    arr = [('edX', 'Founded by Harvard and MIT, edX is home to more than 20 million learners, the majority of top-ranked universities in the world and industry-leading companies.', 'https://www.edx.org/', 'adults', 'general'), ('Coursera', 'Hundreds of free courses give you access to on-demand video lectures, homework exercises, and community discussion forums.', 'https://www.coursera.org/', 'adults', 'general'), ('Docsteach', 'National Archives online resource for teaching with primary documents.', 'https://www.docsteach.org', 'all', 'general')]
+    arr = [('test1', 'Founded by Harvard and MIT, edX is home to more than 20 million learners, the majority of top-ranked universities in the world and industry-leading companies.', 'https://www.edx.org/', 'adults', 'general'), ('test2', 'Hundreds of free courses give you access to on-demand video lectures, homework exercises, and community discussion forums.', 'https://www.coursera.org/', 'adults', 'general')]
     for entry in arr:
         # Prepare SQL query to INSERT a record into the database.
         try:
