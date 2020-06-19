ad10616669bef8e8b913eea7ff358e5730297e90
Jamie Mott <jamie.c.mott@gmail.com>
2020-06-19 08:55:09 -0600

    Trying to connect database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
+import os
+import psycopg2
+
 from flask import Flask, render_template
 app = Flask(__name__)
+
+DATABASE_URL = os.environ['postgres://uxljrqiaodhmkp:26a74ef19a7168996b17cfd2fdfb63fd07eb685db6b13320b9ea943a77dae747@ec2-34-224-229-81.compute-1.amazonaws.com:5432/d34ui9hdrc2rdh']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 @app.route('/')
