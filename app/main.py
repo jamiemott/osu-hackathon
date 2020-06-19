3627013b1f88cee8a855b5715e0a3c63b76fc42e
Jamie Mott <jamie.c.mott@gmail.com>
2020-06-19 15:12:26 -0600

    Trying to insert db data

--- app/main.py
+++ app/main.py
@@ -27,14 +27,13 @@ def links():
     return render_template('links.html')
 
 def insertDB():
-	  # prepare a cursor object using cursor() method
+    # prepare a cursor object using cursor() method
     cursor = db.cursor()
 
     # Prepare SQL query to INSERT a record into the database.
-    sql = """INSERT INTO Websites (name, description, url, age, category) VALUES ('Khan Academy', 'A nonprofit with the mission to provide a free, world-class education for anyone, anywhere.', 'https://www.khanacademy.org/', 'all', 'general'), ('edX', 'Founded by Harvard and MIT, edX is home to more than 20 million learners, the majority of top-ranked universities in the world and industry-leading companies.', 'https://www.edx.org/', 'adults', 'general'), ('Coursera', 'Hundreds of free courses give you access to on-demand video lectures, homework exercises, and community discussion forums.', 'https://www.coursera.org/', 'adults', 'general'), ('Docsteach', 'National Archives online resource for teaching with primary documents.', 'https://www.docsteach.org', 'all', 'general');"""
     try:
-        # Execute the SQL command
-        cursor.execute(sql)
+    # Execute the SQL command
+        cursor.execute(INSERT INTO Websites (name, description, url, age, category) VALUES ('Khan Academy', 'A nonprofit with the mission to provide a free, world-class education for anyone, anywhere.', 'https://www.khanacademy.org/', 'all', 'general'), ('edX', 'Founded by Harvard and MIT, edX is home to more than 20 million learners, the majority of top-ranked universities in the world and industry-leading companies.', 'https://www.edx.org/', 'adults', 'general'), ('Coursera', 'Hundreds of free courses give you access to on-demand video lectures, homework exercises, and community discussion forums.', 'https://www.coursera.org/', 'adults', 'general'), ('Docsteach', 'National Archives online resource for teaching with primary documents.', 'https://www.docsteach.org', 'all', 'general');
         # Commit your changes in the database
         db.commit()
     except:
