51e687fb224233da297ce9d2041e539f17174923
kallytang <tangka@oregonstate.edu>
2020-06-20 18:33:35 -0700

    commenting out import line of main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import os
 import psycopg2
-import requests, json
+
 
 from flask import Flask, render_template, request, redirect
 
-DATABASE_URL = os.environ['DATABASE_URL']
+# DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 app = Flask(__name__)
 
